34f472308f29f5f81eb8e3dbb8e87d5a60e1d557
Your Name <you@example.com>
2022-09-12 14:23:55 +0530

    added:logic for insert

---
+++ src/linkedlist/LinkedListMain.java
@@ -0,0 +1,14 @@
+package linkedlist;
+public class LinkedListMain {
+    public static void main(String[] args) {
+            System.out.println("welcome to linked list program");
+            LinkedList list = new LinkedList();
+            list.append(56);
+            list.append(70);
+            list.show();
+
+            list.insertMiddle(30);
+            list.show();
+        }
+
+    }
